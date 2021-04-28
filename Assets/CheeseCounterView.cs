717340b4fdf247063fe56b89c688e4b33b057439
boyan.evtushok <boyan.evtushok@greent.games>
2021-04-29 00:05:46 +0700

    added cheese counter

---
+++ Assets/CheeseCounterView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheeseCounterView : MonoBehaviour
+{
+    public Player Player;
+
+    public Text Text;
+    void Update()
+    {
+        if (Player)
+        {
+            Text.text = "x" + Player.ateCheeses;
+        }
+    }
+}
